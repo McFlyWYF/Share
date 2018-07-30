4cf90853b0a4c12d8c9918de3fe8fbc5312b663e
wangyufei <1650043869@qq.com>
2018-07-30 18:12:45 +0800

    :art:登录注册逻辑

--- app/src/main/java/com/example/share/LoginActivity.java
+++ app/src/main/java/com/example/share/LoginActivity.java
@@ -3,13 +3,26 @@ package com.example.share;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.LogInListener;
+import cn.bmob.v3.listener.SaveListener;
+import rx.Subscriber;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private TextView register;
@@ -17,32 +30,70 @@ public class LoginActivity extends AppCompatActivity {
     private EditText accountText;
     private EditText passwordText;
 
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this, "88cd8e0280c01d96a6bb4f89e92d0dee");//初始化Bmob
         setContentView(R.layout.activity_login);
+        init();
+    }
 
-        login = (Button) findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private void init() {
+        register = findViewById(R.id.tv_regiester);
+        login = findViewById(R.id.btn_login);
+        accountText = findViewById(R.id.et_account);
+        passwordText = findViewById(R.id.et_password);
 
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+        accountText.setOnClickListener(this);
+        passwordText.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_regiester:
+                Intent intent1 = new Intent(getApplicationContext(), RegiesterActivity.class);
+                startActivityForResult(intent1, REQUEST_SIGNUP);
+                break;
+
+            case R.id.btn_login:
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+//                login();
+                break;
+        }
+    }
 
-        register = (TextView) findViewById(R.id.tv_regiester);
-        register.setOnClickListener(new View.OnClickListener() {
+    private void login(){
+        String account = accountText.getText().toString();
+        String password = passwordText.getText().toString();
+        BmobUser.loginByAccount(account,password , new LogInListener<BmobUser>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegiesterActivity.class);
-                startActivity(intent);
-                finish();
+            public void done(BmobUser user, BmobException e) {
+                if(user != null){
+                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                this.finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/share/RegiesterActivity.java
+++ app/src/main/java/com/example/share/RegiesterActivity.java
@@ -1,25 +1,100 @@
 package com.example.share;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegiesterActivity extends AppCompatActivity {
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+
+public class RegiesterActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private EditText account_et;
     private EditText password_one_et;
     private EditText password_two_et;
-    private TextView return_login;
+    private TextView return_to_login;
     private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regiester);
+        init();
+    }
+
+    public void init() {
+        account_et = (EditText) findViewById(R.id.register_account);
+        password_one_et = (EditText) findViewById(R.id.regiester_password_one);
+        password_two_et = (EditText) findViewById(R.id.regiester_password_two);
+        return_to_login = findViewById(R.id.return_login);
+        register = findViewById(R.id.btn_register);
+
+        account_et.setOnClickListener(this);
+        password_one_et.setOnClickListener(this);
+        password_two_et.setOnClickListener(this);
+        return_to_login.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.return_login:
+                Intent intent1 = new Intent(RegiesterActivity.this, LoginActivity.class);
+                startActivity(intent1);
+                finish();
+                break;
+
+            case R.id.btn_register:
+                final String account = account_et.getText().toString();
+                final String password_one = password_one_et.getText().toString();
+                final String password_two = password_two_et.getText().toString();
+
+                if (account.isEmpty() || password_one.isEmpty()){
+                    Toast.makeText(RegiesterActivity.this,"用户名或密码不能为空",Toast.LENGTH_SHORT).show();
+                }else {
+                    if (account.length() < 6 || password_one.length() < 6){
+                        Toast.makeText(RegiesterActivity.this,"用户名或密码长度要超过6位",Toast.LENGTH_SHORT).show();
+                    }else {
+                        if (!password_one.equals(password_two)){
+                            Toast.makeText(RegiesterActivity.this,"两次密码不一样",Toast.LENGTH_SHORT).show();
+                        }else {
+
+                            User p = new User();
+                            p.setAccount(account);
+                            p.setPassword(password_one);
+                            p.save(new SaveListener<String>() {
+                                @Override
+                                public void done(String s, BmobException e) {
+                                    if (e == null) {
+                                        Toast.makeText(RegiesterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                                        Intent intent2 = new Intent(RegiesterActivity.this, LoginActivity.class);
+                                        startActivity(intent2);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(RegiesterActivity.this, "注册失败" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                            SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                            editor.putString("name", account);
+                            editor.putString("password", password_one);
+                            editor.apply();
+
+                        }
+                    }
+                }
+
+                break;
+        }
     }
 }
