7f1a50581935c262d0f5f4a840c5760fa42fa8cc
wangyufei <1650043869@qq.com>
2018-09-16 19:42:19 +0800

    :art:消息界面

--- app/src/main/java/com/example/share/FirstFragment.java
+++ app/src/main/java/com/example/share/FirstFragment.java
@@ -1,22 +1,52 @@
 package com.example.share;
 
+import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.Request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ezy.ui.view.BannerView;
 
 /**
  * Created by 小飞侠 on 2018/7/28.
  */
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment{
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.layout_first_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_first_fragment, container, false);
+
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+        Toolbar toolbar = (Toolbar) appCompatActivity.findViewById(R.id.id_toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setTitle("");
+
+        }
+        appCompatActivity.setSupportActionBar(toolbar);
+        super.onActivityCreated(savedInstanceState);
     }
 
 }
--- app/src/main/java/com/example/share/MyFragment.java
+++ app/src/main/java/com/example/share/MyFragment.java
@@ -1,8 +1,14 @@
 package com.example.share;
 
+import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,9 +18,123 @@ import android.view.ViewGroup;
 
 public class MyFragment extends Fragment {
 
+    private CardView cardView1,cardView2,cardView3,cardView4,cardView5;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.layout_my_fragment,container,false);
+        cardView1 = view.findViewById(R.id.cardView1);
+        cardView2 = view.findViewById(R.id.cardView2);
+        cardView3 = view.findViewById(R.id.cardView3);
+        cardView4 = view.findViewById(R.id.cardView4);
+        cardView5 = view.findViewById(R.id.cardView5);
+        init();
         return view;
+    }
+
+    private void init(){
+
+        cardView1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    //手指按下
+                    case MotionEvent.ACTION_DOWN:
+                        cardView1.setCardElevation(0);
+                        break;
+
+                    //手指松开
+                    case MotionEvent.ACTION_UP:
+                        cardView1.setCardElevation(20);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        cardView2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    //手指按下
+                    case MotionEvent.ACTION_DOWN:
+                        cardView2.setCardElevation(0);
+                        break;
+
+                    //手指松开
+                    case MotionEvent.ACTION_UP:
+                        cardView2.setCardElevation(20);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        cardView3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    //手指按下
+                    case MotionEvent.ACTION_DOWN:
+                        cardView3.setCardElevation(0);
+                        break;
+
+                    //手指松开
+                    case MotionEvent.ACTION_UP:
+                        cardView3.setCardElevation(20);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        cardView4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    //手指按下
+                    case MotionEvent.ACTION_DOWN:
+                        cardView4.setCardElevation(0);
+                        break;
+
+                    //手指松开
+                    case MotionEvent.ACTION_UP:
+                        cardView4.setCardElevation(20);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        cardView5.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    //手指按下
+                    case MotionEvent.ACTION_DOWN:
+                        cardView5.setCardElevation(0);
+                        break;
+
+                    //手指松开
+                    case MotionEvent.ACTION_UP:
+                        cardView5.setCardElevation(20);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        AppCompatActivity appCompatActivity= (AppCompatActivity) getActivity();
+        Toolbar toolbar= (Toolbar) appCompatActivity.findViewById(R.id.id_toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setTitle("");
+
+        }
+        appCompatActivity.setSupportActionBar(toolbar);
+        super.onActivityCreated(savedInstanceState);
     }
 }
